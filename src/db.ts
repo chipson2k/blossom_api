8311a9f99ca7423bf4966f1a393ea69671e8f21f
ntdung2000 <dung.nt.60ttql@ntu.edu.vn>
2023-08-15 09:54:57 +0700

    api version 1.2

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectToDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://dungvang033:<password>@tutorial.bxfz8fv.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://dungvang033:JbfFmktNtmuHAXK1@tutorial.bxfz8fv.mongodb.net/?retryWrites=true&w=majority"
     );
     if (connection) {
       console.log("Connection established");
