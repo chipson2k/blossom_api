0920153e6f46e848219e15a4fc9d02d8c61a78b6
ntdung2000 <dung.nt.60ttql@ntu.edu.vn>
2023-08-15 11:12:15 +0700

    api version 1.6

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
   try {
+    mongoose.set("strictQuery", false);
     const connection = await mongoose.connect(
       "mongodb+srv://dungvang033:JbfFmktNtmuHAXK1@tutorial.bxfz8fv.mongodb.net/?retryWrites=true&w=majority"
     );
